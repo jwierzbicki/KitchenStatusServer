45dcc14b8f96c0be132fddb59cb2d947d1262651
jwierzbicki <jwierzbicki22@gmail.com>
2019-03-03 10:37:05 +0100

    Add generate shopping list HTTP GET action

--- Controllers/ProductsController.cs
+++ Controllers/ProductsController.cs
@@ -1,5 +1,7 @@
 ﻿using KitchenStatusServer.Models;
 using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
 using System.Linq;
 
 namespace KitchenStatusServer.Controllers
@@ -25,6 +27,33 @@ namespace KitchenStatusServer.Controllers
         {
             var target = dbContext.Products.SingleOrDefault(p => p.Id == id);
             return target;
+        }
+
+        [HttpGet("shoppinglist")]
+        public ProductToBuy[] GenerateShoppingList()
+        {
+            List<Product> allProducts = dbContext.Products.ToList();
+            List<ProductToBuy> shoppingList = new List<ProductToBuy>();
+
+            foreach (var product in allProducts)
+            {
+                if(product.StateCurrent < product.StateMinimal)
+                {
+                    var amount = product.StateCurrent;
+                    var tobuy = 0.0;
+
+                    while(amount < product.StateMinimal)
+                    {
+                        amount += product.UnitQuantity;
+                        tobuy++;
+                    }
+                    tobuy *= product.UnitQuantity;
+                    
+                    shoppingList.Add(new ProductToBuy() { Name = product.Name, Amount = tobuy });
+                }
+            }
+
+            return shoppingList.ToArray();
         }
     }
 }
